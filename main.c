1c98d2e985ddf898a77f53bbdcbba2e00472989a
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-02-20 09:56:25 +0100

    commit

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N_5 
 int main(int argc, char** argv) {
+    int i = 0;
+    int maggiorenne=0;
+    
+    if(maggiorenne>i){
+        printf("Inserisci l' eta di 5 le persone");
+        printf("Inserisci l'eta': &d", p);
+        scanf("%d", &p[i]);
+        i=i+1;
+    }  
+               
+        
+     return (EXIT_SUCCESS);
+}            
+                
+       
 
-    return (EXIT_SUCCESS);
-}
+   
+    
+    
+    
+    
+    
+
 
